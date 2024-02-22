5a9e2246569d6c2752bba40eb089dec06cb03687
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-02-22 15:19:10 +0000

    jkh

---
+++ lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+        style: TextStyle(
+          color:Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg'
+          height: 20,
+          width: 20,
+          ),
+        decoration: BoxDecoration(
+        color: Color(0xffF7F8F8),
+        borderRadius: BorderRadius.circular(10)
+        )
+      ),
+      ),
+    );
+  }
+}
