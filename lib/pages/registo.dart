e91ecde3dec5726e3cf2853eb5a5fd1702972c72
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-02-23 12:17:54 +0000

    ljb

--- lib/pages/registo.dart
+++ lib/pages/registo.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  String? _selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro'),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'First Name'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Last Name'),
+            ),
             TextFormField(
               decoration: InputDecoration(labelText: 'Email'),
               keyboardType: TextInputType.emailAddress,
@@ -19,6 +32,22 @@ class RegisterPage extends StatelessWidget {
             TextFormField(
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
+            ),
+            DropdownButtonFormField<String>(
+              value: _selectedGender,
+              hint: Text('Select Gender'),
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedGender = newValue;
+                });
+              },
+              items: <String>['Male', 'Female', 'Other']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             SizedBox(height: 20),
             ElevatedButton(
