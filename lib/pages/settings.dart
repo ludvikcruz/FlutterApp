12006b49955dfdb433581fd32bcc45ef14f10751
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-03-21 17:02:42 +0000

    g

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,3 @@
-// Em settings_page.dart
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -8,9 +7,61 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Configurações"),
       ),
-      body: Center(
-        child: Text("Aqui vão as configurações"),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Notificações"),
+            subtitle: Text("Novas mensagens, curtidas, e mais"),
+            leading: Icon(Icons.notifications),
+            onTap: () {
+              // Navegar para a página de configurações de notificação
+            },
+          ),
+          ListTile(
+            title: Text("Privacidade e Segurança"),
+            subtitle: Text("Configurações de segurança da conta"),
+            leading: Icon(Icons.lock),
+            onTap: () {
+              // Navegar para a página de configurações de privacidade e segurança
+            },
+          ),
+          ListTile(
+            title: Text("Conta"),
+            subtitle: Text("Configurações da conta, como mudança de senha"),
+            leading: Icon(Icons.person),
+            onTap: () {
+              // Navegar para a página de configurações da conta
+            },
+          ),
+          ListTile(
+            title: Text("Tema"),
+            subtitle: Text("Claro, Escuro, ou Sistema"),
+            leading: Icon(Icons.palette),
+            onTap: () {
+              // Navegar para a página de configurações de tema
+            },
+          ),
+          ListTile(
+            title: Text("Sobre"),
+            subtitle: Text("Informações sobre o aplicativo"),
+            leading: Icon(Icons.info),
+            onTap: () {
+              // Navegar para a página Sobre
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+// onTap: () {
+//   Navigator.push(
+//     context,
+//     MaterialPageRoute(builder: (context) => PrivacySettingsPage()),
+//   );
+// },
